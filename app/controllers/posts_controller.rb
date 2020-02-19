e110bc729ccc3d960495fd26aec3fd0388b4ccac
Maria Le <hp.mars.le@gmail.com>
2020-02-18 22:33:25 -0600

    initial controller, styling, and view setup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,24 @@
 class PostsController < ApplicationController
     def new
+        @post = Post.new
     end
 
     def create
+        @post = Post.new
+        @post.caption = params[:caption]
+        @post.save
     end
 
     def index
+        @posts = Post.all
     end
 
     def show
+        @post = Post.find(params[:id])
     end
 
     def edit
+        @post = Post.find(params[:id])
     end
 
     def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'application#welcome'
+  root to: 'home#index'
   resources :posts
 end
