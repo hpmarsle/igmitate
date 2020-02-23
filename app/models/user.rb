abe876fd1cfc1efeb0d9b25d9c531e3f5561d7b9
Maria Le <hp.mars.le@gmail.com>
2020-02-23 15:10:34 -0600

    add validation for username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
