46891ac6e4c5b2b216cc187eecb315db736d90c9
Maria Le <hp.mars.le@gmail.com>
2020-02-24 07:11:27 -0600

    generate hashtag model and added hashtag associations with user and post models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  has_many :hashtags, through: :posts
   validates :username, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
