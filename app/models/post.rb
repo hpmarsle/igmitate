713c7accb0ebe61ac27a0334925c66dd0058ee56
Maria Le <hp.mars.le@gmail.com>
2020-02-28 13:10:10 -0600

    added validations and error explanations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,12 @@
 class Post < ApplicationRecord
     belongs_to :user
     belongs_to :hashtag
+    validates :caption, presence: true, length: { minimum: 3 }
     # accepts_nested_attributes_for :hashtag
-
+    validates :hashtag, presence: true
     def hashtag_attributes=(hashtag_attribute)
-        # hash_tag = Hashtag.find_or_create_by(hashtag_attribute)
-        self.build_hashtag(hashtag_attribute)
+        hash_tag = Hashtag.find_or_create_by(hashtag_attribute)
+        hash_tag.posts << self
+        # self.build_hashtag(hashtag_attribute)
     end
 end
